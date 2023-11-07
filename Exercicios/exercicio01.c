85aba8468f3115cba2555c8a290ad92228449a32
Michel MD <tkobr10@gmail.com>
2023-11-07 16:36:38 -0300

    Exercicio adicionado

--- Exercicios/exercicio01.c
+++ Exercicios/exercicio01.c
@@ -4,25 +4,35 @@ int main()
 {
     //declaração e atribuição da variável condicao para parada
     int condicao = 0;
-    float nota;
-    printf("\nDigite a nota final do aluno: "); 
-    scanf("%f", &nota);
+    float nota[4], media;
+
+    for (int i = 0; i < 4; i++)
+    {
+        do
+        {
+            printf("\nDigite a nota %d do aluno: ", i); 
+            scanf("%f", &nota[i]);
+        } while (nota[i] < 0 || nota[i] > 10);
+        
+        media += nota[i];
+    }
+    //calculo da média
+    media /= 4;
     //verifica se a condicao é diferente de 1, enquanto for diferente estará no loop
     while (condicao != 1)
     {
         //verifica se a nota é menor 0 ou maior que 10
-        if(nota < 0 || nota > 10)
+        if(media < 0 || media > 40)
         {
-            printf("\nNota invalida! Digite a nota final do aluno: "); 
-            scanf("%f", &nota); 
+            printf("\nNotas invalidas!"); 
         }
         else
         {
-            printf("\nNota valida.\n");
-            if (nota >= 6)
+            printf("\nNotas validas.\n");
+            if (media >= 6)
             {
                 printf("Voce esta aprovado(a)!");
-            } else if (nota >= 5)
+            } else if (media >= 5)
             {
                 printf("Voce esta em recuperacao!");
             } else
