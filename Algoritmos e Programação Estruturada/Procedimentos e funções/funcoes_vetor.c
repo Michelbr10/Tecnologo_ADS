aa35896ce94b0b1483dda82ed752b928334733e0
Michel MD <tkobr10@gmail.com>
2023-11-14 22:59:24 -0300

    testando static da funçao

--- Algoritmos e Programação Estruturada/Procedimentos e funções/funcoes_vetor.c
+++ Algoritmos e Programação Estruturada/Procedimentos e funções/funcoes_vetor.c
@@ -18,20 +18,21 @@ int main()
 {
     int *ponteiro_vetor, *ponteiro_vetor2; //cria um ponteiro para depois apontarmos o endereço do vetor
     ponteiro_vetor = gerar_vetor_randomico(); //atribui ao ponteiro o vetor por meio do endereço do vetor criado pela função "gerar_vetor_randomico()"
-    ponteiro_vetor2 = gerar_vetor_randomico(); //chamando a função outra vez e atribuindo o seu endereço
-
 
     for (int n = 0; n < 10; n++)
     {
         printf("\n %i", ponteiro_vetor[n]);
     }
-    for (int m = 0; m < 10; m++)
-    {
-        printf("\n %i", ponteiro_vetor2[m]);
-    }
-
     printf("\nEndereco do vetor passada pela funcao %p", ponteiro_vetor); //endereço na memoria do vetor no main()
-    printf("\nEndereco do vetor passada pela funcao %p", ponteiro_vetor2); //ambos endereços são iguais, porque o static não permite que o vetor seja alterado
+    //criando novo vetor e chamando a função, veremos que sera atribuido o mesmo endereço do anterior e seus valores
+    {
+        ponteiro_vetor2 = gerar_vetor_randomico(); //chamando a função outra vez e atribuindo o seu endereço
+        for (int m = 0; m < 10; m++)
+        {
+            printf("\n %i", ponteiro_vetor2[m]);
+        }
+        printf("\nEndereco do vetor passada pela funcao %p", ponteiro_vetor2); //ambos endereços são iguais, porque o static não permite que o vetor seja alterado
+    }
 
     return 0;
 }
