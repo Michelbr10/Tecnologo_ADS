7d142ff516b923eba0e363e15c240da9c9a6d236
Michel MD <tkobr10@gmail.com>
2023-11-14 22:58:44 -0300

    verificando static em um vetor

--- Algoritmos e Programação Estruturada/Procedimentos e funções/funcoes_vetor.c
+++ Algoritmos e Programação Estruturada/Procedimentos e funções/funcoes_vetor.c
@@ -3,7 +3,7 @@
 
 int* gerar_vetor_randomico()
 {
-    static int vetor[10];
+    static int vetor[10]; //static não deixa que esse vetor seja alterado, mesmo a função sendo chamada outra vez
     
     for (int i = 0; i < 10; i++)
     {
@@ -16,14 +16,22 @@ int* gerar_vetor_randomico()
 
 int main()
 {
-    int *ponteiro_vetor; //cria um ponteiro para depois apontarmos o endereço do vetor
+    int *ponteiro_vetor, *ponteiro_vetor2; //cria um ponteiro para depois apontarmos o endereço do vetor
     ponteiro_vetor = gerar_vetor_randomico(); //atribui ao ponteiro o vetor por meio do endereço do vetor criado pela função "gerar_vetor_randomico()"
+    ponteiro_vetor2 = gerar_vetor_randomico(); //chamando a função outra vez e atribuindo o seu endereço
+
 
     for (int n = 0; n < 10; n++)
     {
         printf("\n %i", ponteiro_vetor[n]);
     }
+    for (int m = 0; m < 10; m++)
+    {
+        printf("\n %i", ponteiro_vetor2[m]);
+    }
+
     printf("\nEndereco do vetor passada pela funcao %p", ponteiro_vetor); //endereço na memoria do vetor no main()
+    printf("\nEndereco do vetor passada pela funcao %p", ponteiro_vetor2); //ambos endereços são iguais, porque o static não permite que o vetor seja alterado
 
     return 0;
 }
