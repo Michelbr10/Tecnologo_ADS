7d0249dff904364c031eec0bcd2da4f1628fc391
Michel MD <tkobr10@gmail.com>
2024-05-18 17:32:03 -0300

    atualizado algoritmo de juros

--- Exercicios/juros.cpp
+++ Exercicios/juros.cpp
@@ -1,13 +1,17 @@
+//Bibliotecas
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
+//Classes
 class Juros_Compostos {
     public:
+        //Atributos
         float capital_inicial;
         float taxa_juros;
         float tempo;
-
+        //Métodos
         float valor_futuro(){
             float futuro = capital_inicial* std::pow(1 + taxa_juros, tempo);
             return futuro;
@@ -19,14 +23,28 @@ class Juros_Compostos {
         }
 };
 
+//Funções
+void exibe_retornos(string tipo_calculo, float valor){
+    cout << setprecision(2) << fixed;
+    cout << tipo_calculo << valor << endl;
+}
+
+float guarda_valores(string parametro){
+    float valor;
+    cout << parametro << endl;
+    cin >> valor;
+    return valor;
+}
+
 int main(){
     Juros_Compostos investimento;
 
-    investimento.capital_inicial = 100000;
-    investimento.taxa_juros = 0.01;
-    investimento.tempo = 120;
+    investimento.capital_inicial = guarda_valores("Capital inicial: ");
+    investimento.taxa_juros = guarda_valores("Taxa de juros: ");
+    investimento.tempo = guarda_valores("Tempo em meses: ");
     
-    cout << "Valor futuro: " << investimento.valor_futuro() << endl;
-    cout << "Valor presente: " << investimento.valor_presente() << endl;
+    exibe_retornos("Valor presente: ", investimento.valor_presente());
+    exibe_retornos("Valor futuro: ", investimento.valor_futuro());
+
     return 0;
 }
